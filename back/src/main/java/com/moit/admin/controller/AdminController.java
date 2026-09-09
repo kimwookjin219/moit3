5fc241b2e1dc5fca6c221e27277974ebf5fe94c4
kimwookjin219 <heros21982@gmail.com>
2026-09-09 17:26:01 +0900

    cicd

--- back/src/main/java/com/moit/admin/controller/AdminController.java
+++ back/src/main/java/com/moit/admin/controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.moit.admin.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.moit.member.dto.UserDto;
 import com.moit.member.service.UserService;
